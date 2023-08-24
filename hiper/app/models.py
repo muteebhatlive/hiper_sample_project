ec9c9cf404b869a98227ae51bfb5eee4f278c1c6
Muteeb Bhat <muteebhat7@gmail.com>
2023-08-24 16:38:00 +0530

    -models created -serializers created

--- hiper/app/models.py
+++ hiper/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Transaction(models.Model):
+    transaction_id = models.CharField(max_length=10, unique=True)
+    bank_name = models.CharField(max_length=100)
+    datetime = models.DateTimeField()
+    amount = models.IntegerField()
+     
